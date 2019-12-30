1e4f6c9cf86d9b8dee9b4cb8aaf9e1778499f00a
underflow101 <ikarus125@gmail.com>
2019-12-30 19:21:47 +0900

    updates

---
+++ JSONMarshal/marshalJSON.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+)
+
+type status int
+type Deadline struct {
+	time.Time
+}
+
+const (
+	UNKNOWN = iota
+	TODO
+	DONE
+)
+
+func NewDeadline(t time.Time) *Deadline {
+	return &Deadline(t)
+}
+
+type Task struct {
+	Title    string
+	Status   status
+	Deadline *Deadline
+}
+
+func main() {
+	t := Task{
+		"Laundry",
+		DONE,
+		NewDeadline(time.Date(2020, time.January, 16, 23, 59, 59, 0, time.UTC)),
+	}
+	b, err := json.Marshal(t)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println(string(b))
+}
