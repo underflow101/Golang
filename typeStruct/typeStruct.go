481ba347f117d74092c91433ef795213abd2c45b
underflow101 <ikarus125@gmail.com>
2019-12-30 17:42:09 +0900

    Create typeStruct.go

---
+++ typeStruct/typeStruct.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"time"
+)
+
+type status int
+type ByteSize float64
+
+type task struct {
+	title string
+	done  bool
+	due   *time.Time
+}
+
+type task2 struct {
+	title  string
+	status status
+	due    *time.Time
+}
+
+const (
+	UNKNOWN status = 0
+	TODO    status = 1
+	DONE    status = 2
+)
+
+const (
+	BAD status = iota
+	SOSO
+	GOOD
+)
+
+const (
+	minsu = iota
+	chulsoo
+	minhee
+)
+
+const (
+	_           = iota
+	KB ByteSize = 1 << (10 * iota)
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
+
+func main() {
+	var myTask = task{"work", false, nil}
+
+}
