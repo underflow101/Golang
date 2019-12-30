b507cc48efcc5b930639479589c48f7716764165
underflow101 <ikarus125@gmail.com>
2019-12-30 13:57:25 +0900

    Create stringMultiSet.go

---
+++ stringMultiSet/stringMultiSet.go
@@ -0,0 +1,33 @@
+package StringMultiSet
+
+import "strings"
+
+// MultiSet is a data type idealisation
+type MultiSet map[string]int
+
+//Insert Func
+func (m MultiSet) Insert(val string) {
+	m[val]++
+}
+
+// Erase func
+func (m MultiSet) Erase(val string) {
+	if m[val] < 2 {
+		delete(m, val)
+	} else {
+		m[val]--
+	}
+}
+
+// Count func
+func (m MultiSet) Count(val string) int {
+	return m[val]
+}
+
+func (m MultiSet) String() string {
+	s := "{ "
+	for val, count := range m {
+		s += strings.Repeat(val+" ", count)
+	}
+	return s + " }"
+}
