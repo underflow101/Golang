3707cd59993345220e52463666bfb558e7705776
underflow101 <ikarus125@gmail.com>
2019-12-31 11:11:54 +0900

    Update unmarshalJSON.go

--- JSONunMarshal/unmarshalJSON.go
+++ JSONunMarshal/unmarshalJSON.go
@@ -1,9 +1,10 @@
 // JSON Unserialization (역직렬화)
 
 package main
+
 import (
+	"encoding/json"
 	"fmt"
-	"JSON"
 	"log"
 	"time"
 )
@@ -29,6 +30,22 @@ type Task struct {
 	Deadline *Deadline
 }
 
+func unmarshalJSON(b []byte) Task {
+	t := Task{}
+	err := json.Unmarshal(b, &t)
+	if err != nil {
+		log.Println(err)
+		t.Title = "ERROR"
+		t.Status = UNKNOWN
+		return t
+	}
+	return t
+}
+
 func main() {
-	
+	b := []byte(`{"Title":"Buy Milk","Status":2,"Deadline":"2020-05-26T23:59:59Z"}`)
+	t := unmarshalJSON(b)
+	fmt.Println(t.Title)
+	fmt.Println(t.Status)
+	fmt.Println(t.Deadline.UTC())
 }
