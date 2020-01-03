c8d8f49cb91102a7434e863f6494043ab8e194fa
Dongwon Paek <ikarus125@gmail.com>
2020-01-03 17:10:22 +0900

    Create Gob.go

---
+++ Gob/Gob.go
@@ -0,0 +1,31 @@
+// Gob
+
+package main
+import (
+	"fmt"
+	"encoding/gob"
+	"bytes"
+)
+
+func main() {
+	var b bytes.Buffer
+	enc := gob.NewEncoder(&b)
+	data := map[string]string{"N":"J"}
+	if err := enc.Encode(data); err != nil {
+		fmt.Println(err)
+	}
+	const width = 16
+	for start := 0; start < len(b.Bytes()); start += width {
+		end := start + width
+		if end > len(b.Bytes()) {
+			end = len(b.Bytes())
+		}
+		fmt.Printf("%x\n", b.Bytes()[start:end])
+	}
+	dec := gob.NewDecoder(&b)
+	var restored map[string]string
+	if err := dec.Decode(&restored); err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(restored)
+}
