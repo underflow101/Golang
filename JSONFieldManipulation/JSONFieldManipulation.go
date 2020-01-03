5771434451b578807372a32c541787250ee50abb
Dongwon Paek <ikarus125@gmail.com>
2020-01-03 16:49:09 +0900

    Create JSONFieldManipulation.go

---
+++ JSONFieldManipulation/JSONFieldManipulation.go
@@ -0,0 +1,22 @@
+// JSON Field Manipulation
+
+package main
+import (
+	"fmt"
+	"encoding/json"
+)
+
+type Fields struct {
+	VisibleField string
+	InvisibleField string
+}
+
+func main() {
+	f := &Fields{"a", "b"}
+	b, _ := json.Marshal(struct {
+		*Fields
+		InvisibleField string `json:",omitempty"`
+		Additional string
+	}{Fields: f, Additional: "c"})
+	fmt.Println(string(b))
+}
