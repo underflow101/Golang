565acd389f965c26b3e58ad1a451b7923ffbd184
Dongwon Paek <ikarus125@gmail.com>
2020-01-06 17:12:24 +0900

    Update dataAccessObject.go

--- dataAccessObject/dataAccessObject.go
+++ dataAccessObject/dataAccessObject.go
@@ -1,2 +1,118 @@
-// Data Access Object
+// Data Access Object for RESTful API
 
+package main
+import (
+	"errors"
+	"fmt"
+)
+
+type ID string
+type status int
+
+type Deadline struct {
+	time.Time
+}
+
+func NewDeadline(t time.Time) *Deadline {
+	return &Deadline{t}
+}
+
+const (
+	UNKNOWN = iota
+	TODO
+	DONE
+)
+
+type Task struct {
+	Title string `json:"title,omitempty"`
+	Status status `json:"status,omitempty"`
+	Deadline *Deadline `json:"deadline,omitempty"`
+	Priority int `json:"priority,omitempty"`
+	SubTasks []Task `json:"subTasks,omitempty"`
+}
+
+type IncludeSubTasks Task
+
+func (t IncludeSubTasks) indentedString(prefix string) string {
+	str := prefix + Task(t).String()
+	for _, st := range t.SubTasks {
+		str += "\n" + IncludeSubTasks(st).indentedString(prefix + " ")
+	}
+	return str
+}
+
+func (t IncludeSubTasks) String() string {
+	return t.indentedString("")
+}
+
+func (t Task) String() string {
+	check := "v"
+	if t.Status == TODO {
+		check = "-"
+	} else if t.Status == UNKNOWN {
+		check = "?"
+	}
+	return fmt.Sprintf("[%s] %s %s", check, t.Title, t.Deadline)
+}
+
+func PrintStringer(data fmt.Stringer) {
+	fmt.Print(data.String())
+}
+
+type DataAccess interface {
+	Get(id ID) (task.Task, error)
+	Put(id ID, t task.Task) error
+	Post(t task.Task) (ID, error)
+	Delete(id ID) error
+}
+
+type MemoryDataAccess struct {
+	tasks map[ID]task.Task
+	nextID int64
+}
+
+func NewMemoryDataAccess() DataAccess {
+	return &MemoryDataAccess {
+		tasks: map[ID]task.Task{},
+		nextID: int64(1),
+	}
+}
+
+var ErrTaskNotExist = errors.New("task does not exist")
+
+func (m *MemoryDataAccess) Get(id ID) (task.Task, error) {
+	t, exists := m.tasks[id]
+	if !exists {
+		return task.Task{}, ErrTaskNotExist
+	}
+	return t, nil
+}
+
+func (m *MemoryDataAccess) Put(id ID, t task.Task) error {
+	if _, exists := m.tasks[id]; !exists {
+		return ErrTaskNotExist
+	}
+	m.tasks[id] = t
+	return nil
+}
+
+func (m *MemoryDataAccess) Post(t task.Task) (ID, error) {
+	id := ID(fmt.Sprint(m.nextID))
+	m.nextID++
+	m.tasks[id] = t
+	return id, nil
+}
+
+func (m *MemoryDataAccess) Delete(id ID) error {
+	if _, exists := m.tasks[id]; !exists {
+		return ErrTaskNotExist
+	}
+	delete(m.tasks, id)
+	return nil
+}
+
+
+
+func main() {
+
+}
