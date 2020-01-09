091a0f7665a90c8668484cfed740d68bfee6c0bc
Dongwon Paek <ikarus125@gmail.com>
2020-01-09 16:28:35 +0900

    Update pipelineBasic.go

--- pipeline/pipelineBasic.go
+++ pipeline/pipelineBasic.go
@@ -4,6 +4,8 @@ package main
 
 import "fmt"
 
+type IntPipe func(<-chan int) <-chan int
+
 func PlusOne(in <-chan int) <-chan int {
 	out := make(chan int)
 	go func() {
@@ -15,15 +17,30 @@ func PlusOne(in <-chan int) <-chan int {
 	return out
 }
 
+func Chain(ps ...IntPipe) IntPipe {
+	return func(in <-chan int) <-chan int {
+		c := in
+		for _, p := range ps {
+			c = p(c)
+		}
+		return c
+	}
+}
+
 func main() {
 	c := make(chan int)
+	//PlusTwo := Chain(PlusOne, PlusOne, PlusOne)
+	PlusFive := Chain(PlusOne, PlusOne, PlusOne, PlusOne, PlusOne)
 	go func() {
 		defer close(c)
 		c <- 5
 		c <- 3
 		c <- 8
 	}()
-	for num := range PlusOne(PlusOne(PlusOne(c))) {
-		fmt.Println(num)
+	// for num := range PlusTwo(c) {
+	// 	fmt.Println(num)
+	// }
+	for n := range PlusFive(c) {
+		fmt.Println(n)
 	}
 }
