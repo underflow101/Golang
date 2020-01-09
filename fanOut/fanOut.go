c0361e4e4641a1f7bbcf85272b04760132a38e02
Dongwon Paek <ikarus125@gmail.com>
2020-01-09 16:48:23 +0900

    Create fanOut.go

---
+++ fanOut/fanOut.go
@@ -0,0 +1,44 @@
+// Fan-out, Fan-in
+
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func FanIn(ins ...<-chan int) <-chan int {
+	out := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(len(ins))
+	for _, in := range ins {
+		go func(in <-chan int) {
+			defer wg.Done()
+			for num := range in {
+				out <- num
+			}
+		}(in)
+	}
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func main() { // fan-out
+	c := make(chan int)
+	for i := 0; i < 3; i++ {
+		go func(i int) {
+			for n := range c {
+				time.Sleep(1)
+				fmt.Println(i, n)
+			}
+		}(i)
+	}
+	for i := 0; i < 10; i++ {
+		c <- i
+	}
+	close(c)
+}
