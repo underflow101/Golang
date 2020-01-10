004b91126a082f0942a2afadf061d990e8f1f959
Dongwon Paek <ikarus125@gmail.com>
2020-01-10 11:15:47 +0900

    Create doNotWaitCh.go

---
+++ doNotWaitCh/doNotWaitCh.go
@@ -0,0 +1,48 @@
+// skip if no input, get input when there is input
+
+package main
+
+import "fmt"
+
+func FanIn3(in1, in2, in3 <-chan int) <-chan int {
+	out := make(chan int)
+	// openCnt := 3
+	// closeChan := func(c *<-chan int) bool {
+	// 	*c = nil
+	// 	openCnt--
+	// 	return openCnt == 0
+	// }
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case n := <-in1:
+				fmt.Println(n)
+			case n := <-in2:
+				fmt.Println(n)
+			case n := <-in3:
+				fmt.Println(n)
+			default:
+				fmt.Println("Data Not Ready. Skipping...")
+			}
+		}
+	}()
+	return out
+}
+
+func main() {
+	c1, c2, c3 := make(chan int), make(chan int), make(chan int)
+	sendInts := func(c chan<- int, begin, end int) {
+		defer close(c)
+		for i := begin; i < end; i++ {
+			c <- i
+		}
+	}
+	go sendInts(c1, 11, 14)
+	go sendInts(c2, 21, 23)
+	go sendInts(c3, 31, 35)
+	for n := range FanIn3(c1, c2, c3) {
+		fmt.Print(n, ", ")
+	}
+	fmt.Println("END!")
+}
