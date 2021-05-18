a0e890b260d0d9a65ed9a1393b0de9c898893f00
underflow101 <ikarus125@gmail.com>
2021-05-18 14:15:06 +0900

    feat: Add Websocket using Gorilla package
    Signed-off-by: underflow101 <ikarus125@gmail.com>

---
+++ websocketGorilla/main.go
@@ -0,0 +1,44 @@
+/**
+* main.go
+* Dev. Dongwon Paek
+* Executive Dev. Namsu Lee
+*
+* Go Agent package for
+ */
+
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+var addr = flag.String("addr", ":22635", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
+
+func main() {
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
+	})
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
